881b794e4b42adda0993e243cef7d85ded274889
유정주 <dbwjdwn9216@gmail.com>
2024-04-23 21:56:38 +0900

    [#3] 저장소와 관련된 테스트를 LaunchRepositoryTests로 이동

---
+++ RefactorMoti/RefactorMotiTests/Launch/LaunchRepositoryTests.swift
@@ -0,0 +1,21 @@
+//
+//  LaunchRepositoryTests.swift
+//  RefactorMotiTests
+//
+//  Created by 유정주 on 4/23/24.
+//
+
+import XCTest
+
+final class LaunchRepositoryTests: XCTestCase {
+
+    // MARK: 앱의 현재 정보
+    
+    func test_given_모든_경우에_when_앱_정보에서_현재_버전을_가져오면_then_nil이_아니다() throws { }
+    
+    // MARK: 파이어베이스의 버전 정보
+    
+    func test_given_파이어베이스_저장소가_존재할_때_when_최신_버전을_가져오면_then_nil이_아니다() throws { }
+    
+    func test_given_파이어베이스_저장소가_존재할_때_when_강제업데이트_버전을_가져오면_then_nil이_아니다() throws { }
+}
