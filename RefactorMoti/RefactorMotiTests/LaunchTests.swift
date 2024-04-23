546e2b7b1f0171018ae36cad57a046f1d431f47c
유정주 <dbwjdwn9216@gmail.com>
2024-04-23 21:36:44 +0900

    [#3] Launch 화면에 필요한 테스트 목록 정의

---
+++ RefactorMoti/RefactorMotiTests/LaunchTests.swift
@@ -0,0 +1,47 @@
+//
+//  LaunchTests.swift
+//  RefactorMotiTests
+//
+//  Created by 유정주 on 4/23/24.
+//
+
+import XCTest
+
+final class LaunchTests: XCTestCase {
+
+    // MARK: 앱의 현재 정보
+    
+    func given_모든_경우에_when_앱_정보에서_현재_버전을_가져오면_then_nil이_아니다() throws {
+        
+    }
+    
+    // MARK: 파이어베이스의 버전 정보
+    
+    func given_파이어베이스_저장소가_존재할_때_when_최신_버전을_가져오면_then_nil이_아니다() throws {
+        
+    }
+    
+    func given_파이어베이스_저장소가_존재할_때_when_강제업데이트_버전을_가져오면_then_nil이_아니다() throws {
+        
+    }
+    
+    // MARK: 현재 버전과 최신 버전 비교 결과
+    
+    func given_현재버전과_최신버전이_존재할_때_when_현재버전이_최신버전_미만이라면_then_true_업데이트가_가능하다() throws {
+        
+    }
+    
+    func given_현재버전과_최신버전이_존재할_때_when_현재버전과_최신버전이_같다면_then_false_최신_버전이다() throws {
+        
+    }
+    
+    // MARK: 현재 버전과 강제 업데이트 버전 비교 결과
+    
+    func given_현재버전과_강제업데이트버전이_존재할_때_when_현재버전이_강제업데이트버전_이하라면_then_true_업데이트가_필요하다() throws {
+        
+    }
+    
+    func given_현재버전과_강제업데이트버전이_존재할_때_when_현재버전이_강제업데이트버전_초과라면_then_false_업데이트가_필요없다() throws {
+        
+    }
+}
