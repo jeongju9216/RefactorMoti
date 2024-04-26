7f2ca9f208f543ed5074fcc6aefb39a6914c7093
유정주 <dbwjdwn9216@gmail.com>
2024-04-26 21:52:50 +0900

    [#5] 계정 인증에 필요한 AuthUseCase 테스트 코드 작성

---
+++ RefactorMoti/RefactorMotiTests/Auth/AuthUseCaseTests.swift
@@ -0,0 +1,33 @@
+//
+//  AuthUseCaseTests.swift
+//  RefactorMotiTests
+//
+//  Created by 유정주 on 4/26/24.
+//
+
+import XCTest
+
+final class AuthUseCaseTests: XCTestCase {
+
+    // MARK: 난수 생성
+    
+    func test_난수_생성_요청을_했을_때_100개의_난수가_모두_다르면_true() throws { }
+    
+    // MARK: 로그인
+    
+    func test_로그인을_시도했을_때_성공이면_반환값이_true() throws { }
+    
+    func test_로그인을_시도했을_때_실패면_반환값이_false() throws { }
+    
+    // MARK: 로그아웃
+    
+    func test_로그아웃을_시도했을_때_성공이면_반환값이_true() throws { }
+    
+    func test_로그아웃을_시도했을_때_실패면_반환값이_false() throws { }
+    
+    // MARK: 회원 탈퇴
+    
+    func test_회원탈퇴를_시도했을_때_성공이면_반환값이_true() throws { }
+    
+    func test_회원탈퇴를_시도했을_때_실패면_반환값이_false() throws { }
+}
