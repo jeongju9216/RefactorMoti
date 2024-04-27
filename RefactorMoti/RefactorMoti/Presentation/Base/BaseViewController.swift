81eee470720f0ce51d18d6fe1fd8fd63385c9bf9
유정주 <dbwjdwn9216@gmail.com>
2024-04-27 17:20:19 +0900

    [#8] BaseViewController 추가
    모든 ViewController가 상속하는 BaseViewController를 정의했습니다.

---
+++ RefactorMoti/RefactorMoti/Presentation/Base/BaseViewController.swift
@@ -0,0 +1,32 @@
+//
+//  BaseViewController.swift
+//  RefactorMoti
+//
+//  Created by 유정주 on 4/27/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    // MARK: - Setup
+    
+    /// View의 Attribute 속성을 설정
+    func setUpAttribute() { }
+    
+    /// View의 Subview를 설정
+    func setUpSubview() { }
+    
+    /// View의 Subview Constraints를 설정
+    func setUpConstraint() { }
+    
+    /// View의 Combine Binding을 설정
+    func setUpBinding() { }
+}
