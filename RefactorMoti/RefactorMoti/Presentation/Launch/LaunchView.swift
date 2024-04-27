02ae149a20367c23c84919df9d9536efcb54cccb
유정주 <dbwjdwn9216@gmail.com>
2024-04-27 22:02:41 +0900

    [#8] LaunchView UI 구현
    version label의 텍스트는 추후 현재 버전으로 대치 예정입니다.

--- RefactorMoti/RefactorMoti/Presentation/Launch/LaunchView.swift
+++ RefactorMoti/RefactorMoti/Presentation/Launch/LaunchView.swift
@@ -5,13 +5,63 @@
 //  Created by 유정주 on 4/27/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
 
 final class LaunchView: BaseView {
+    
+    // MARK: - UI
+    
+    private let motiIconImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(resource: .appIconRound)
+        return view
+    }()
+    private let versionLabel: UILabel = {
+        let view = UILabel()
+        view.text = "version"
+        return view
+    }()
+    
     
     // MARK: - Setup
     
     override func setUpAttribute() {
         backgroundColor = .systemBackground
+    }
+    
+    override func setUpSubview() {
+        addSubview(motiIconImageView)
+        addSubview(versionLabel)
+    }
+    
+    override func setUpConstraint() {
+        motiIconImageView.snp.makeConstraints {
+            $0.size.equalTo(Size.appIconRound)
+            $0.center.equalToSuperview()
+        }
+        versionLabel.snp.makeConstraints {
+            $0.bottom.equalTo(safeAreaLayoutGuide.snp.bottom).offset(-Metric.VersionLabel.bottomOffset)
+            $0.centerX.equalToSuperview()
+        }
+    }
+}
+
+
+// MARK: - Constant
+
+private extension LaunchView {
+    
+    enum Metric {
+        
+        enum VersionLabel {
+            
+            static let bottomOffset = 20
+        }
+    }
+    
+    enum Size {
+        
+        static let appIconRound = CGSize(width: 120, height: 120)
     }
 }
