7c241a67f9cffe72997b41de49c5b7b9fb76b494
유정주 <dbwjdwn9216@gmail.com>
2024-04-28 21:15:11 +0900

    [#16] 로그인 화면 UI 구현
    로그인 버튼은 애플 로그인을 적용할 예정이지만, 현재는 기본 버튼을 추가하였습니다.
    현재 로그인 액션은 무조건 성공하도록 처리할 예정이고, 추후 계정 인증을 넣을 예정입니다.

---
+++ RefactorMoti/RefactorMoti/Presentation/Login/LoginView.swift
@@ -0,0 +1,73 @@
+//
+//  LoginView.swift
+//  RefactorMoti
+//
+//  Created by 유정주 on 4/28/24.
+//
+
+import UIKit
+
+final class LoginView: BaseView {
+    
+    // MARK: - UI
+    
+    private let motiIconImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(resource: .appIconRound)
+        return view
+    }()
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        var configuration = UIButton.Configuration.filled()
+        configuration.title = Text.login
+        configuration.baseBackgroundColor = .lightGray
+        button.configuration = configuration
+        return button
+    }()
+    
+    
+    // MARK: - Setup
+    
+    override func setUpAttribute() {
+        backgroundColor = .systemBackground
+    }
+    
+    override func setUpSubview() {
+        addSubview(motiIconImageView)
+        addSubview(loginButton)
+    }
+    
+    override func setUpConstraint() {
+        motiIconImageView.atl
+            .size(Size.appIconRound)
+            .center(equalTo: safeAreaLayoutGuide)
+        loginButton.atl
+            .centerX(equalTo: safeAreaLayoutGuide.centerXAnchor)
+            .horizontal(equalTo: safeAreaLayoutGuide, constant: Metric.LoginButton.offset)
+            .bottom(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -Metric.LoginButton.offset)
+    }
+}
+
+
+// MARK: - Constant
+
+private extension LoginView {
+    
+    enum Metric {
+        
+        enum LoginButton {
+            
+            static let offset: CGFloat = 20
+        }
+    }
+    
+    enum Size {
+        
+        static let appIconRound = CGSize(width: 120, height: 120)
+    }
+    
+    enum Text {
+        
+        static let login = "로그인"
+    }
+}
