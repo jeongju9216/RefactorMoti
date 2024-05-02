3bfa6c1616d321521e5caf3eb8a0ae1e78ad605a
유정주 <dbwjdwn9216@gmail.com>
2024-05-02 18:07:52 +0900

    [#37] AchievementRepository 정의
    AchievementRepository는 Achievement를 파이어베이스 저장소로 부터 가져와 UseCase로 반환하는 역할입니다.

---
+++ RefactorMoti/RefactorMoti/Data/Repository/AchievementRepository.swift
@@ -0,0 +1,15 @@
+//
+//  AchievementRepository.swift
+//  RefactorMoti
+//
+//  Created by 유정주 on 5/2/24.
+//
+
+import Foundation
+
+struct AchievementRepository: AchievementRepositoryProtocol { 
+    
+    func fetchAchievements() async throws -> [Achievement] {
+        return []
+    }
+}
