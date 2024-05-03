f6bb97b28277bb232280241c784e532d88d0195c
유정주 <dbwjdwn9216@gmail.com>
2024-05-03 21:52:58 +0900

    [#36] FetchAchievementsUseCaseTests 테스트 코드 작성
    현재는 간단한 시나리오만 작성하였습니다.

---
+++ RefactorMoti/RefactorMotiTests/Domain/UseCase/FetchAchievementsUseCaseTests.swift
@@ -0,0 +1,15 @@
+//
+//  FetchAchievementsUseCaseTests.swift
+//  RefactorMotiTests
+//
+//  Created by 유정주 on 5/3/24.
+//
+
+import XCTest
+
+final class FetchAchievementsUseCaseTests: XCTestCase {
+
+    func test_fetchAchievements_수행이_성공하면_Achievements를_반환한다() { }
+
+    func test_fetchAchievements_수행이_실패하면_에러를_발생시킨다() { }
+}
