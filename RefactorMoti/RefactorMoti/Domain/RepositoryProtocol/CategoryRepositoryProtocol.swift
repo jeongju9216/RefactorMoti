547ed06857ec54f7fccbbd5c2ab574d677653891
유정주 <dbwjdwn9216@gmail.com>
2024-05-04 13:58:19 +0900

    [#51] CategoryRepository 구현

---
+++ RefactorMoti/RefactorMoti/Domain/RepositoryProtocol/CategoryRepositoryProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryRepositoryProtocol.swift
+//  RefactorMoti
+//
+//  Created by 유정주 on 5/4/24.
+//
+
+import Foundation
+
+protocol CategoryRepositoryProtocol {
+    
+    func fetchCategories() async throws -> [String]
+}
