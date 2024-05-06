d858f577ea1631d0f17f9f88738545c4a806da3c
유정주 <dbwjdwn9216@gmail.com>
2024-05-06 10:11:24 +0900

    [#65] addCategory 테스트 코드 작성

--- RefactorMoti/RefactorMotiTests/Data/Repository/CategoryRepositoryTests.swift
+++ RefactorMoti/RefactorMotiTests/Data/Repository/CategoryRepositoryTests.swift
@@ -13,6 +13,9 @@ final class CategoryRepositoryTests: XCTestCase {
     private let defaultCategories = [CategoryItem(id: 0, name: "전체"), CategoryItem(id: 1, name: "미설정")]
     private let customCategory = CategoryItem(id: 2, name: "음식")
     
+    
+    // MARK: - Fetch Categories
+    
     func test_기본_카테고리만_있을_때_fetchCategories_수행에_성공하면_기본_category_리스트를_반환한다() async throws {
         // given
         let repository = DefaultCategoryRepositoryStub()
@@ -38,4 +41,19 @@ final class CategoryRepositoryTests: XCTestCase {
     }
     
     func test_fetchCategories_수행에_실패하면_에러를_발생시킨다() async throws { }
+    
+
+    // MARK: - Add Category
+    
+    func test_addCategory에_성공하면_true를_반환한다() async throws {
+        // given
+        let repository = DefaultCategoryRepositoryStub()
+        let category = CategoryItem(id: 3, name: "카테고리")
+        
+        // when
+        let isSuccess = await repository.addCategory(category)
+        
+        // then
+        XCTAssertTrue(isSuccess)
+    }
 }
