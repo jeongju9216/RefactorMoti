62d815ad59dddf78c2d9b4edf6cd0c4606daf1f0
유정주 <dbwjdwn9216@gmail.com>
2024-05-06 11:21:50 +0900

    [#66] AddCategoryUseCaseTests 작성

---
+++ RefactorMoti/RefactorMotiTests/Domain/UseCase/AddCategoryUseCaseTests.swift
@@ -0,0 +1,15 @@
+//
+//  AddCategoryUseCaseTests.swift
+//  RefactorMotiTests
+//
+//  Created by 유정주 on 5/6/24.
+//
+
+import XCTest
+
+final class AddCategoryUseCaseTests: XCTestCase {
+
+    func test_카테고리_추가에_성공하면_true를_반환한다() async throws { }
+    
+    func test_카테고리_추가에_실패하면_false를_반환한다() async throws { }
+}
