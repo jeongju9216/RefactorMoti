dbed3ed5a7f5d240a8524df4360071c5b02ee3cb
유정주 <dbwjdwn9216@gmail.com>
2024-05-11 15:17:50 +0900

    [#91] CreateDefaultCategoriesUseCase 구현

---
+++ RefactorMoti/RefactorMoti/Domain/UseCase/CreateDefaultCategoriesUseCase.swift
@@ -0,0 +1,34 @@
+//
+//  CreateDefaultCategoriesUseCase.swift
+//  RefactorMoti
+//
+//  Created by 유정주 on 5/11/24.
+//
+
+import Foundation
+
+protocol CreateDefaultCategoriesUseCaseProtocol {
+    
+    func execute() async -> Bool
+}
+
+struct CreateDefaultCategoriesUseCase: CreateDefaultCategoriesUseCaseProtocol {
+    
+    // MARK: - Interface
+    
+    func execute() async -> Bool {
+        await repository.createDefaultCategories()
+    }
+    
+    
+    // MARK: - Attribute
+    
+    private let repository: CategoryRepositoryProtocol
+    
+    
+    // MARK: - Initializer
+    
+    init(repository: CategoryRepositoryProtocol = CategoryRepository()) {
+        self.repository = repository
+    }
+}
