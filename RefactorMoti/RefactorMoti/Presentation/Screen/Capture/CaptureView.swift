dff3b64a9715ad0a5c07be6cf569f6c450ee517b
유정주 <dbwjdwn9216@gmail.com>
2024-05-25 18:17:16 +0900

    [#143] CaptureVIew에 title label 추가
    Capture 화면임을 확인할 수 있도록 title label을 표시하였습니다.

--- RefactorMoti/RefactorMoti/Presentation/Screen/Capture/CaptureView.swift
+++ RefactorMoti/RefactorMoti/Presentation/Screen/Capture/CaptureView.swift
@@ -5,6 +5,34 @@
 //  Created by 유정주 on 5/25/24.
 //
 
-import Foundation
+import UIKit
+import PinLayout
 
-final class CaptureView: BaseView {}
+final class CaptureView: BaseView {
+    
+    // MARK: - UI
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.text = "Capture"
+        label.textAlignment = .center
+        return label
+    }()
+    
+    
+    // MARK: - Life Cycle
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        label.pin.all()
+    }
+    
+    
+    // MARK: - Setup
+    
+    override func setUpSubview() {
+        super.setUpSubview()
+        addSubview(label)
+    }
+}
