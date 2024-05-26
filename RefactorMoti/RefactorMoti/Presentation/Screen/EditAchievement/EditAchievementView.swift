a25acd56cab6849648b7fc278347c2fd5c76b37c
유정주 <dbwjdwn9216@gmail.com>
2024-05-26 20:45:06 +0900

    [#134] EditAchievementView애 본문을 입력하는 body textview 추가

--- RefactorMoti/RefactorMoti/Presentation/Screen/EditAchievement/EditAchievementView.swift
+++ RefactorMoti/RefactorMoti/Presentation/Screen/EditAchievement/EditAchievementView.swift
@@ -34,6 +34,11 @@ final class EditAchievementView: BaseView {
         view.backgroundColor = JDColor.gray
         return view
     }()
+    private let bodyTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = Text.sampleBody
+        return textView
+    }()
     
     
     // MARK: - Life Cycle
@@ -42,7 +47,7 @@ final class EditAchievementView: BaseView {
         super.layoutSubviews()
         flexBox.pin.all(pin.safeArea)
         flexBox.flex.layout()
-        previewView.pin.hCenter().vCenter(-Size.systemButton.height)
+        previewView.pin.center()
     }
     
     
@@ -66,6 +71,10 @@ final class EditAchievementView: BaseView {
             flex.addItem().grow(1).define { flex in
                 flex.addItem(previewView).width(100%).aspectRatio(1)
             }
+            
+            flex.addItem().define { flex in
+                flex.addItem(bodyTextView).marginHorizontal(Metric.horizontalOffset)
+            }
         }
     }
 }
@@ -79,6 +88,7 @@ private extension EditAchievementView {
         
         static let buttonPointSize = 21.0
         static let systemButton = CGSize(width: 44, height: 44)
+        static let bodyTextViewHeight = 100.0
     }
     
     enum Metric {
@@ -99,5 +109,29 @@ private extension EditAchievementView {
     enum Text {
         
         static let textFieldPlaceholder = "제목을 입력하세요."
+        static let sampleBody = """
+        친구는 세월의 도둑이다.
+        봄부터 흐르는 물은 겨울이 되어도 얼지 않듯이
+        마음에서 우러나오는 우적은 역경이 닥친다고 해서 식지 않는다.
+        불행했을 때 만난 친구는 가장 소중히 여겨야 한다.
+        행복했을 때 함께 기쁨을 누리는 친구보다
+        힘들 떄 슬픔을 덜어지는 친구를 더 많이 신뢰할 수 있다.
+        부유했을 때는 친구를 사귀기 쉽지만,
+        어려울 때는 눈을 씻고 찾아봐도 친구를 찾기 어렵다.
+        누군가 진정한 우정을 나누고 있다 할지라도 그 우정을 지키려면 오랜 시간이 걸린다.
+        짧은 시간 안에 많은 친구를 얻기란 불가능하다. 왜냐하면
+        그들은 진정한 친구가 아니기 때문이다.
+        친구란 말하자면 또 하나의 자신이다.
+        친구를 보면 그 사람을 알 수 있다.
+        현명한 사람과 어리석은 사람은 서로 어울리지 않는다.
+        친구는 나를 동정하는 자가 아니라 나를 돕는 자다.
+        서로가 고통을 덜어 주지 않는다면 우리는 무엇을 위해 사는 것일까?
+        우리는 모두 적막한 세계를 떠도는 나그네다.
+        그 여정에서 찾을 수 있는 최고의 선물은 바로 믿음직한 벗이다.
+        좋은 벗은 충격을 덜어주는 완충장치와도 같은 것이며
+        인생의 길에서 충동을 피할 수 있도록 도와준다.
+        우정만이 세상을 하나로 만들 수 있다.
+        우정은 영혼의 결혼이다.
+        """
     }
 }
